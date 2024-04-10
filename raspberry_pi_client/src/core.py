732555db3f26e68cc17a5b248cd72d9b76cb268c
Marc Vila <mvila@worldsensing.com>
2024-04-10 18:39:25 +0200

    fix observable_property in raspberry_pi_client

--- raspberry_pi_client/src/core.py
+++ raspberry_pi_client/src/core.py
@@ -8,17 +8,16 @@ from __init__ import URL_ONTOLOGY_DEPLOYMENT_LOCAL, URL_ONTOLOGY_DEPLOYMENT_CLOU
 
 BASE_URL = URL_ONTOLOGY_DEPLOYMENT_CLOUD
 SENSORS_ENDPOINT_URL = "/sensors/"
-SENSOR_OBSERVATIONS_ENDPOINT_URL = "/observations/"  # TODO Change to /sensor-observations/
+SENSOR_OBSERVATIONS_ENDPOINT_URL = "/observations/"
 OBSERVATIONS_ENDPOINT_URL = "/observations/"
 OBSERVABLE_PROPERTIES_ENDPOINT_URL = "/observable-properties/"
 
 
 def post_sensor_observation(value, sensor_name=ONTOLOGY_SENSOR_NAME,
                             observable_property=ONTOLOGY_OBSERVABLE_PROPERTY):
-    print(f"Sending POST to create an observation")
     url = f"{BASE_URL}{OBSERVATIONS_ENDPOINT_URL}"
-    print(url)
-    body = {"sensor_name": sensor_name, "observable_property": observable_property,
+    print(f"Sending POST to create an observation {url}")
+    body = {"sensor_name": sensor_name, "observable_property_name": observable_property,
             "time_start": utils.get_current_time(), "value_int": value}
     print(body)
 
@@ -30,5 +29,5 @@ def post_sensor_observation(value, sensor_name=ONTOLOGY_SENSOR_NAME,
         return observation["id"]
     except:
         pass
-    
+
     return None
